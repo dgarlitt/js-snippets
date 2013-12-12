71990ff25d6f2b7f5d8add90981930b8e1e68249
Dan Arlitt <dan@nonfatmedia.com>
2013-12-12 13:11:48 -0800

    Number formatting binding snippet

---
+++ bindings/number-formatting-binding.js
@@ -0,0 +1,42 @@
+ko.bindingHandlers.number = {
+	update: function(element, valueAccessor, allBindingsAccessor) {
+		var defaults = ko.bindingHandlers.number.defaults,
+			aba = allBindingsAccessor,
+			unwrap = ko.utils.unwrapObservable,
+			value = unwrap(valueAccessor()) || valueAccessor(),
+			result = '',
+			numarray;
+
+		var	separator = unwrap(aba().separator) || defaults.separator,
+			decimal = unwrap(aba().decimal) || defaults.decimal,
+			precision = unwrap(aba().precision) || defaults.precision,
+			symbol = unwrap(aba().symbol) || defaults.symbol,
+			after = unwrap(aba().after) || defaults.after;
+
+        value = parseFloat(value) || 0;
+
+        if (precision > 0)
+            value = value.toFixed(precision)
+
+            numarray = value.toString().split('.');
+
+        for (var i = 0; i < numarray.length; i++) {
+            if (i == 0) {
+                result += numarray[i].replace(/(\d)(?=(\d\d\d)+(?!\d))/g,'$1'+separator);
+            } else {
+                result += decimal + numarray[i];
+            }
+        }
+
+		result = (after) ? result += symbol : symbol + result;
+
+	    ko.bindingHandlers.text.update(element, function() { return result; });
+	},
+	defaults: {
+		separator: ',',
+		decimal: '.',
+		precision: 0,
+		symbol: '',
+		after: false
+	}
+};
